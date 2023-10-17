f3b4252a1e773ba6512a2480108f4bf19086ddc4
callhervida <vidaa.g1999@gmail.com>
2023-10-17 14:10:50 +0330

    added book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Book(models.Model):
+    """A Model for blog post's information including content's title, content
+    author and Publication Date"""
+
+    title = models.CharField(blank=True, null=True, max_length=100)
+
+    price = models.DecimalField(max_digits=10, decimal_places=1)
+
+    quantity = models.IntegerField(blank=True, null=True)
+
+    description = models.TextField(blank=True, null=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
